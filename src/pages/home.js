bcaa262b5321519fe1e1cd05bd19cc494f56897d
Rishabh Tripathy <112729251+RishabhTripathy@users.noreply.github.com>
2024-07-01 16:05:11 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,29 +1,29 @@
 // HomePage.js
-import React from 'react';
-import Player from '../components/player';
-import Footer from '../components/footer';
-import Who from '../components/Who';
-import Nav from '../components/nav';
-import WorkTogether from '../components/workTogether';
+import React from "react";
+import Player from "../components/player";
+import Footer from "../components/footer";
+import Who from "../components/Who";
+import Nav from "../components/nav";
+import WorkTogether from "../components/workTogether";
 
 function HomePage({ projects }) {
   return (
-    <div className="flex flex-col bg-black h-full w-full items-center gap-4 md:gap-8">
-          <Nav />
-
-      {projects.map((project) => (
-        <div key={project.id} className="w-full bg-black h-full">
-          <Player
-            title={project.title}
-            description={project.description}
-            url={project.link}
-          />
-        </div>
-      ))}
-          <Who />
-          <WorkTogether/>
-          <Footer />
-
+    <div className="h-full w-full bg-white "> 
+      <div className="flex flex-col bg-black  items-center gap-4 md:gap-8">
+        <Nav />
+        {projects.map((project) => (
+          <div key={project.id} className="w-full bg-black h-full">
+            <Player
+              title={project.title}
+              description={project.description}
+              url={project.link}
+            />
+          </div>
+        ))}
+        <Who />
+      </div>
+      <WorkTogether />
+      <Footer />
     </div>
   );
 }
