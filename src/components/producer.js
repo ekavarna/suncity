8c7d52ba4b044aeae3716d01fa75cd2aba7b3d08
Rishabh Tripathy <rishabhtripathy@gmail.com>
2024-11-14 13:26:57 +0530

    producer updated

--- src/components/producer.js
+++ src/components/producer.js
@@ -33,7 +33,7 @@ export default function Producer({ isOpen, onClose }) {
       <div className="flex flex-col  md:flex-row items-center justify-center gap-8 max-w-[820px]  my-auto py-4">
         <div className="relative aspect-[3/4] w-full max-w-xs ">
           <img
-            src="https://cms.suncitystudios.in/uploads/director_3253b8e772.webp"
+            src="https://cms.suncitystudios.in/uploads/zuleikha_gupta_30aef8f8f2.jpeg"
             alt="Director Rahul Bharti  Sun City Studios"
             className="h-full w-full object-cover     rounded-xl shadow-lg"
           />
@@ -41,31 +41,22 @@ export default function Producer({ isOpen, onClose }) {
         <div className=" basis-1/2 space-y-2 md:text-left  lg:max-w-full max-w-xs">
           <h1 className=" font-light text-theme-pink  uppercase">Producer</h1>
           <h1 className="font-Armavir text-2xl text-theme-yellow  uppercase">
-            Zuleikha
+            Zuleikha Gupta{" "}
           </h1>
           <div className="font-light text-left md:text-justify text-sm space-y-4 ">
             <p>
-              Rahul Bharti is a seasoned director whose humour is so intelligent
-              and subtle it’s practically wearing reading glasses. He has a
-              filmmaking style that’s like a well-tailored suit unassuming yet
-              unforgettable. His approach is modest, but don’t let that fool
-              you; it packs a punch sharper than a box of witty one-liners.
-              Bharti excels at taking everyday scenarios and giving them a twist
-              so unique you’d think he’s got a secret deal with the universe.
+              Zuleikha has wrangled over 14 directors across time zones and
+              temperaments; it’s basically the Olympics of filmmaking.
             </p>
             <p>
-              His films are like your favourite pair of jeans, comfortably
-              relatable but with just enough flair to make everyone take a
-              second look. With a keen eye for detail and an uncanny
-              understanding of human quirks, he crafts stories that resonate so
-              deeply you might start wondering if he’s been eavesdropping on
-              your life.
+              At this point, she speak fluent
+              ‘director-who-changes-everything-at-the-last-minute’.
             </p>
             <p>
-              Over the years, he has crafted stories for brands like Make My
-              Trip, Swiggy, BoAt, Shaadi.com, Center Fresh, Shark Tank, Zomato,
-              Jio Cinema, Vadilal, Dunzo, PayTM, Hotstar, Pizza Hut, Winzo
-              amongst others.
+              And after collaborating with over 14 agencies and 55 brands to
+              produce over 150 spots, she’s also now fluent in ‘brand-speak’,
+              ‘make the logo bigger’ and can translate ‘we need it yesterday’
+              into 10 languages.
             </p>
           </div>
         </div>
