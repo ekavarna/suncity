aa22ddfd9654a14fae0e1f3fa6a2e51d544952e9
Rishabh Tripathy <112729251+RishabhTripathy@users.noreply.github.com>
2026-09-09 12:45:40 +0530

    Update copyright year in footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -54,7 +54,7 @@ export default function Footer() {
         </div>
       </div>
       <div className="flex justify-between font-light px-4 pb-8 text-sm mx-4 md:mx-8 items-center">
-        <h1 className="">2024. Copyright. All rights reserved.</h1>
+        <h1 className="">2026. Copyright. All rights reserved.</h1>
         <h1 className="">Sun City Studios</h1>
       </div>
     </div>
